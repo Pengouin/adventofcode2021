a07ac528b770aeb565efbed675bdac3ae00ced94
Antoine <antoine@isstar.be>
2021-12-01 12:36:15 +0100

    Initial commit

---
+++ Day1/puzzle1.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const _ = require('underscore');
+const fs = require('fs');
+
+let depths = fs.readFileSync('Day1/inputs/input1.txt').toString().split("\n");
+depths = _.map(depths, function(item) { return parseInt(item); })
+depths = _.filter(depths, function(item) { return !_.isNaN(item); });
+
+let previousMeasurement;
+let increased = 0;
+
+_.each(depths, function(currentMeasurement) {
+    if (!_.isUndefined(previousMeasurement)) {
+        if (previousMeasurement < currentMeasurement) increased++;
+    }
+    previousMeasurement = currentMeasurement;
+});
+
+console.log("Result :", increased);
---
+++ Day1/puzzle2.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const _ = require('underscore');
+const fs = require('fs');
+
+let depths = fs.readFileSync('Day1/inputs/input1.txt').toString().split("\n");
+depths = _.map(depths, function(item) { return parseInt(item); });
+depths = _.filter(depths, function(item) { return !_.isNaN(item); });
+
+let increased = 0;
+
+for (let index = 4; index < depths.length; index ++) {
+    const currentSum = depths[index] + depths[index - 1] + depths[index - 2];
+    const previousSum = depths[index - 1] + depths[index - 2] + depths[index - 3];
+
+    if (currentSum > previousSum) increased++;
+}
+
+console.log("Result :", increased);
