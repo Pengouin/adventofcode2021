80275d4bdbc6d2680ee5ce801a21feb6f3243352
Antoine <antoine@isstar.be>
2021-12-07 11:30:40 +0100

    Day7

---
+++ Day7/part2.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const _ = require('underscore');
+const fs = require('fs');
+
+let crabs = fs.readFileSync('Day7/inputs/input7.txt').toString().split(",");
+crabs = _.map(crabs, function(item) { return parseInt(item); });
+
+let maxPosition = Math.max.apply(null, crabs);
+
+let costs = [];
+for (let i = 0; i <= maxPosition; i++) {
+    costs.push(_.reduce(crabs, function(memo, position) {
+        let cost = Math.abs(i - position);
+        return memo + Math.abs(cost * (cost + 1) / 2);
+    }, 0));
+}
+
+let result = Math.min.apply(null, costs);
+console.log("Result :", result);
